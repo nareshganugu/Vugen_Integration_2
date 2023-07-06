1d9a8c705dad477bfdb11af541a68a9627854170
Naresh Personal Email <nareshganugu@gmail.com>
2023-07-06 10:38:13 -0400

    edit mesage

--- Action.c
+++ Action.c
@@ -4,7 +4,7 @@ Action()
 	lr_start_transaction("S01_SCEE_TC03_Check_Eligibility");
 	
 
-	lr_output_message("Successfull Messages");
+	lr_output_message("Successfull Message");
 	
     
    	lr_end_transaction("S01_SCEE_TC03_Check_Eligibility", LR_FAIL);
